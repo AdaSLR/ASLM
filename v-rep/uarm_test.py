bfe96151182690dac492b15aee07305476b1804b
mattroz <matveyrozanov@gmail.com>
2018-04-13 00:35:26 +0300

    Implemented UARM manipulator motors handling

--- v-rep/uarm_test.py
+++ v-rep/uarm_test.py
@@ -12,27 +12,40 @@
 import vrep
 import numpy as np
 
+
+class UARM(object):
+	def __init__(self, client_id):
+		self.motor_handlers = []
+		for i in range(0,4):
+			error, vrep_motor_handler = vrep.simxGetObjectHandle(client_id, 'uarm_motor' + str(i+1), vrep.simx_opmode_oneshot_wait)
+			if error == vrep.simx_return_ok:
+				self.motor_handlers.append(vrep_motor_handler)
+			else:
+				print('Error getting motor handlers, ERR: ' + str(error))
+				return
+
+
 def main():
-	print ('Program started')
 	vrep.simxFinish(-1) 
-	clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5) # Connect to V-REP
+	# Connect to V-REP via sockets
+	clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
 	if clientID == -1:
-    	print ('Failed connecting to remote API server')
-    	return -1	
+		print ('Failed connecting to remote API server')
+		return -1	
 	print ('Connected to remote API server')
 
-    # Now try to retrieve data in a blocking fashion (i.e. a service call):
-    res, objs = vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_blocking)
+	# Now try to retrieve data in a blocking fashion (i.e. a service call):
+	res, objs = vrep.simxGetObjects(clientID, vrep.sim_handle_all, vrep.simx_opmode_blocking)
 
-    # Now retrieve streaming data (i.e. in a non-blocking fashion):
-    vrep.simxGetIntegerParameter(clientID,vrep.get_joint_state,vrep.simx_opmode_streaming) # Initialize streaming
+	# Now retrieve streaming data (i.e. in a non-blocking fashion):
+	vrep.simxGetIntegerParameter(clientID, vrep.get_joint_state, vrep.simx_opmode_streaming) # Initialize streaming
 
-    # Now send some data to V-REP in a non-blocking fashion:
+	# Now send some data to V-REP in a non-blocking fashion:
 	vrep.simxAddStatusbarMessage(clientID,'Python training script have been connected.',vrep.simx_opmode_oneshot)
 
-    # Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
-    vrep.simxGetPingTime(clientID)
+	# Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+	vrep.simxGetPingTime(clientID)
 
-    # Now close the connection to V-REP:
-    vrep.simxFinish(clientID)
+	# Now close the connection to V-REP:
+	vrep.simxFinish(clientID)
 	return 1
