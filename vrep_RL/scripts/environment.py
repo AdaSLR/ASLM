05519440ac775a95ed14243dc19e02908b1e1c80
mattroz <matveyrozanov@gmail.com>
2018-04-16 00:44:23 +0300

    Implemented connection establishing

--- vrep_RL/scripts/environment.py
+++ vrep_RL/scripts/environment.py
@@ -1,4 +1,4 @@
-from v-rep.lib import vrep
+from lib import vrep
 import numpy as np
 import time
 
@@ -9,19 +9,25 @@ class VREPEnv(object):
 	reinforcement learning algorithms research using V-REP.
 	"""
 
-	def __init__(self, clientID):
-		self.client_id = clientID
+	def __init__(self):
 		self._low_degrees_bound = 0
 		self._low_radians_bound = 0
 		self._high_degrees_bound = 180
 		self._high_radians_bound = np.pi
-		self.camera_handlers = []
+		# Get client id from the V-REP server-side
+		self.conn_handler = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+		if self.conn_handler == -1:
+			print('Failed connecting to the remote API server')
+			return -1
+		print('Succesfully connected to the renote API server...')
+
 		# Get cameras from V-REP
 		# vs_0 - global view,
 		# vs_1 - details basket,
 		# vs_2 - destination basket,
+		self.camera_handlers = []
 		for i in range(3):
-			err, handler = vrep.simxGetObjectHandle(self.client_id, 'vc_' + str(i), vrep.simx_opmode_oneshot_wait)
+			err, handler = vrep.simxGetObjectHandle(self.conn_handler, 'vs_' + str(i), vrep.simx_opmode_oneshot_wait)
 			if err == vrep.simx_return_ok:
 				self.camera_handlers.append(handler)
 			else:
