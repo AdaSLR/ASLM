80a720337f803d0173c8239f6840beaad7c2539f
mattroz <matveyrozanov@gmail.com>
2018-04-16 01:48:54 +0300

    Implemented state getter, embedded uarm in the env Imlemented state getter, which returns four motors' current positions and gripper state; decided that uarm entity should be embedded into Environment class for the sake of encapsulation.

--- vrep_RL/scripts/environment.py
+++ vrep_RL/scripts/environment.py
@@ -1,4 +1,5 @@
 from lib import vrep
+from uarm import UARM
 import numpy as np
 import time
 
@@ -22,6 +23,13 @@ class VREPEnv(object):
 			return -1
 		print('Succesfully connected to the renote API server...')
 
+		# Get UARM entity
+		# Implemented it in the dict style for future purposes
+		# (if there're several robots in the env, call them like 
+		# env.robot['uarm'], env.robot['universal_robotics'], etc) 
+		uarm = UARM(self.conn_handler)
+		self.robot = {'uarm': uarm}	
+
 		# Get cameras from V-REP
 		# vs_0 - global view,
 		# vs_1 - details basket,
@@ -35,5 +43,29 @@ class VREPEnv(object):
 				print('Error getting camera handlers, ERR:', err)
 				return
 
+
+	def state(self, degrees=True):
+		_state = []
+		# Get motors' positions
+		for motor in self.robot['uarm'].motor_handlers:
+			err, joint_position = vrep.simxGetJointPosition(self.conn_handler, motor, vrep.simx_opmode_streaming)
+			if err == vrep.simx_return_ok or err == vrep.simx_return_novalue_flag:
+				joint_position *= 180.0/np.pi 
+				_state.append(round(joint_position, 4))
+			else:
+				print('Error getting joint positions, ERR:', err)
+				return -1
+		
+		# Get gripper state
+		err, gripper_state = vrep.simxGetIntegerSignal(self.conn_handler, self.robot['uarm'].gripper_handler, vrep.simx_opmode_streaming)
+		if err == vrep.simx_return_ok or err == vrep.simx_return_novalue_flag:
+			_state.append(gripper_state)
+		else:
+			print('Error getting gripper state, ERR:', err)
+			_state.append(-1)
+		return _state
+
+
 	def finish(self):
 		vrep.simxFinish(self.conn_handler)
+
